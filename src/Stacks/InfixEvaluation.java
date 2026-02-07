f7e99d1cfcb5b626daa9e2a142b9f438f2d0b5f4
mrudula <mrudulasai3606@gmail.com>
2026-02-07 13:36:02 +0530

    Stack Infix

--- src/Stacks/InfixEvaluation.java
+++ src/Stacks/InfixEvaluation.java
@@ -1,4 +1,71 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class InfixEvaluation {
-}
+    public static void main(String[] args) {
+        String s = "9-(5+3)*4/6";
+        Stack<Integer> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            int asci = (int) ch;
+            if(asci>=48 && asci<=57){
+                val.push(asci-48);
+            }
+            else if(op.size()==0 || ch=='(' || op.peek()=='(') op.push(ch);
+            else if(ch==')'){
+                while(op.peek()!='('){
+                    int val2 = val.pop();
+                    int val1 = val.pop();
+                    if(op.peek()=='+') val.push(val1+val2);
+                    if(op.peek()=='-') val.push(val1-val2);
+                    if(op.peek()=='*') val.push(val1*val2);
+                    if(op.peek()=='/') val.push(val1/val2);
+                    op.pop();
+                }
+                op.pop();
+            }
+            else{
+                if(ch=='+' || ch == '-'){
+                int val2 = val.pop();
+                int val1 = val.pop();
+
+                    if(op.peek()=='+') val.push(val1+val2);
+                    if(op.peek()=='-') val.push(val1-val2);
+                    if(op.peek()=='*') val.push(val1*val2);
+                    if(op.peek()=='/') val.push(val1/val2);
+                    op.pop();
+                    op.push(ch);
+                }
+                else{
+                    if(ch=='*' || ch=='/'){
+                        if(op.peek()=='*' || op.peek()=='/'){
+                            int val2 = val.pop();
+                            int val1 = val.pop();
+                            if(op.peek()=='*') val.push(val1*val2);
+                            if(op.peek()=='/') val.push(val1/val2);
+                            op.pop();
+                            op.push(ch);
+                        }
+                        else{
+                            op.push(ch);
+                        }
+                    }
+                }
+            }
+        }
+         while(val.size()>1){
+             int val2 = val.pop();
+             int val1 = val.pop();
+                 if(op.peek()=='+') val.push(val1+val2);
+                 if(op.peek()=='-') val.push(val1-val2);
+                 if(op.peek()=='*') val.push(val1*val2);
+                 if(op.peek()=='/') val.push(val1/val2);
+                 op.pop();
+         }
+        System.out.println(val.peek());
+        }
+    }
+
